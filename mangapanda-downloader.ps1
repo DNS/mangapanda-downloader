8e1d28705a2750d6ae7ae52ca0bb57af5d96f004
dns <dsirait@aol.com>
2015-03-11 03:07:35 +0700

    sync

--- mangapanda-downloader.ps1
+++ mangapanda-downloader.ps1
@@ -1,10 +1,8 @@
 
-# YOU MUST SPECIFY $manga_name, $chapter_start and $chapter_end
+# YOU MUST SPECIFY $manga_name, eg: (naruto, bleach)
 
 $manga_name = 'naruto'
-$chapter_start = 1
-$chapter_end = 700
-$local_drive = 'd:\Manga'
+
 
 
 
@@ -17,22 +15,46 @@ $local_drive = 'd:\Manga'
 ###############################
 
 
-$url = "http://www.mangapanda.com/$manga_name"
+
+$local_drive = 'C:\'
+$mangapanda = "http://www.mangapanda.com/"
 $local_path = "$local_drive\$manga_name"
+$manga_url = "$mangapanda$manga_name"
 
 $webclient = New-Object System.Net.WebClient
 
+$chapter_string = $webclient.DownloadString("$manga_url")
+$result = $chapter_string -match '<a href="/naruto/(\d+?)">'
+$end_chapter = $matches[1]
+
+$result = $chapter_string -match '<th>Date Added</th>\n</tr>\n<tr>\n<td>((.|\n)*)'
+$chapter_list = $matches[1]
+
+
+#write-output $chapter_list
+$result = $chapter_list -match '<a href="/(.*?)">'
+$chapter_part = $matches[1]
+
+$page_url = "$mangapanda$chapter_part"
+write-host $page_url
+$page_string = $webclient.DownloadString($page_url)
+$result = $page_string -match '</select> of (\d+?)</div>'
+$page_total = $matches[1]
+
+write-host $page_total
+
+
+<#
 For($j=$chapter_start; $j -le $chapter_end; $j++) {
 	$total_pages = 2
 	Write-Host "Chapter: $j"
 	For($i=1; $i -le $total_pages; $i++) {
 		
-		Try {
-			$html_string = $webclient.DownloadString("$url/$j/$i")
-		} Catch [System.Net.WebException] {
-			#$error[0]
-			$html_string = $webclient.DownloadString("$url/$j/$i")
-		}
+		
+		$html_string = $webclient.DownloadString("$url/$j/$i")
+		
+        
+        
 		
 		$result = $html_string -match ' of (\d+?)</div>'
 		$total_pages = $matches[1]
@@ -44,16 +66,12 @@ For($j=$chapter_start; $j -le $chapter_end; $j++) {
 		$file = "$local_path\$j-$i.jpg"
 		Write-Host "$file of $total_pages"
 		
-		Try {
-			$webclient.DownloadFile($remote_file, $file)
-		} Catch [System.Net.WebException] {
-			#$error[0]
-			#Write-Error "Failed to download: $remote_file"
-			$webclient.DownloadFile($remote_file, $file)
-		}
+		
+		$webclient.DownloadFile($remote_file, $file)
+		
 	}
 }
-
+#>
 
 #Write-Host $file
 
